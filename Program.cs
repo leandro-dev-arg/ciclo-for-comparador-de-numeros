98243b5d8a37dba715583db1b9ec1ce6a8602b68
leandro <leandro.dev.arg@gmail.com>
2023-12-18 12:46:34 -0300

    primer subida undidad 5-ejercicio 2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace U5_programa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int a,nuevonumero;
+            a=0;
+            for (int x = 0; x < 10; x++)
+            {
+             Console.WriteLine("Ingresa un numero por favor");
+             nuevonumero=int.Parse(Console.ReadLine());
+                if(nuevonumero>a){
+                    a=nuevonumero;
+                }
+            }
+            Console.WriteLine("el numero mas alto es "+a);
+        
+        }
+    }
+}
